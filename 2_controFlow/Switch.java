fa7d00b40bf85586fba4a772f907cfb059f82464
meda123 <mrt62@cornell.edu>
2017-08-02 20:59:30 -0700

    Oops, added break to Switch statement

--- 2_controFlow/Switch.java
+++ 2_controFlow/Switch.java
@@ -7,12 +7,16 @@ public class Switch {
 
             case 'L':
                 System.out.println("Messi shoots to the left and scores");
+            break;
 
             case 'R':
                 System.out.println("Messi shoots to the right and misses the goal!");
+            break;
 
             case 'C':
                 System.out.println("Messi shoots down the center, but the keeper blocks it!");
+            break;
+
             default:
                 System.out.println("Messi is in position...");
 
