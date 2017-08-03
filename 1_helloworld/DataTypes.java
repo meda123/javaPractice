b155759d43b97b9a10688be9386ba945dae7d035
meda123 <mrt62@cornell.edu>
2017-08-02 20:24:54 -0700

    Added Java practice files. Hello World and control flow practice

---
+++ 1_helloworld/DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        System.out.println(50);
+        System.out.println(100);
+        System.out.println("I rock!!");
+        System.out.println("I world about time!!");
+
+    }
+}
---
+++ 2_controFlow/ifElseIf.java
@@ -0,0 +1,17 @@
+public class ifElseIf {
+    public static void main(String[] args) {
+
+        int round; 
+
+        if (round > 12) {
+
+            System.out.println("The match is over!");
+        } else if (round > 0) {
+            System.out.println("The match is underway");
+        } else {
+            System.out.println("The boxing match hasn't started yet");
+        }
+
+    }
+    
+}
