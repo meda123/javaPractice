395d32f454732243bd2c54052b62eb9915b7c9fd
meda123 <mrt62@cornell.edu>
2017-08-03 10:22:33 -0700

    Completed basic calculator project

---
+++ 3_classes/Calculator.java
@@ -0,0 +1,68 @@
+// This calculator program allows a user to add, subract, mutiply, divide, and 
+// find the remainder of an operation. 
+
+class Calculator {
+    
+  public Calculator() {
+    
+  }
+  
+  public int add(int a, int b) {
+    return a + b;
+  }
+  
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+  
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+  
+  public int divide(int a, int b) {
+    if (b == 0){
+        System.out.println("Error! Dividing by zero is not allowed");
+        return 0;
+    } else {
+      return a / b;
+    }
+
+}
+    
+  public int modulo(int a, int b) {
+    if (b == 0) {
+      System.out.println("Error! Dividing my zero is not allowed");
+      return 0;
+    } else {
+      return a % b;
+    }
+  }
+    
+  public static void main(String[] args) {
+    
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+    System.out.println(myCalculator.multiply(2, 7));
+    System.out.println(myCalculator.divide(4, 0));
+    System.out.println(myCalculator.modulo(7, 3));
+          
+    }
+    
+  }
+
+/*
+Expected output of the program:: 
+
+12
+34
+14
+Error! Dividing by zero is not allowed
+0
+1
+
+*/
+    
+  
+  
+  
